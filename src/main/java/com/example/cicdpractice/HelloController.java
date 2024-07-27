88d59de1cfd4adc201a157da91f08bbdda1a5ba1
honee8583 <honee8583@naver.com>
2024-07-27 15:39:11 +0900

    Hello Controller

--- src/main/java/com/example/cicdpractice/HelloController.java
+++ src/main/java/com/example/cicdpractice/HelloController.java
@@ -1,17 +1,26 @@
 package com.example.cicdpractice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class HelloController {
+    @Value("${server.env}")
+    private String env;
+
     @GetMapping("/hello")
     public String hello() {
 
         log.debug("feat10");
 
         return "hello nginx testing";
+    }
+
+    @GetMapping("/env")
+    public String profile() {
+        return env;
     }
 }
