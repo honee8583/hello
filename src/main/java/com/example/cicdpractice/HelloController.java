5a99c808fc6933298ccc941880e1258b41e13285
honee8583 <honee8583@naver.com>
2024-07-27 15:43:51 +0900

    Hello Controller

--- src/main/java/com/example/cicdpractice/HelloController.java
+++ src/main/java/com/example/cicdpractice/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.cicdpractice;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +14,28 @@ public class HelloController {
     @Value("${server.env}")
     private String env;
 
-    @GetMapping("/hello")
-    public String hello() {
+    @Value("${server.port}")
+    private String serverPort;
 
-        log.debug("feat10");
+    @Value("${server.serverAddress}")
+    private String serverAddress;
 
-        return "hello nginx testing";
+    @Value("${serverName}")
+    private String serverName;
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck() {
+        Map<String, String> responseData = new HashMap<>();
+        responseData.put("serverName", serverName);
+        responseData.put("serverAddress", serverAddress);
+        responseData.put("serverPort", serverPort);
+        responseData.put("env", env);
+
+        return ResponseEntity.ok(responseData);
     }
 
     @GetMapping("/env")
-    public String profile() {
-        return env;
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
     }
 }
